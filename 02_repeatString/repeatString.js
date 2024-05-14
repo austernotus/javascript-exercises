7f09a388a0d08cb008c62e97619098326e64380b
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-14 20:14:16 +0100

    finished 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
+
+    if (number < 0){
+        return "ERROR"
+    }
+    else {
+        let finalString = ""
+
+        for (let i = 0; i < number; i++){
+            finalString = finalString.concat(text);
+        }
+    
+        return finalString
+    }
+
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
