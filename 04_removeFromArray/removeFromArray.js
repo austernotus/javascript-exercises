b0dbd689d39312455653feb6c7ca946304646960
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-14 20:37:11 +0100

    started removeFromArray exercise
    testing solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toRemove, toRemove2, toRemove3) {
 
+    finalArray = []
+
+    const index = array.indexOf(toRemove)
+
+    fin = array.splice(index, 1)
+    return fin
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
