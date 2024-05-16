dee363071e1e337a526c8910c99a866e40a96c1a
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-16 15:36:56 +0100

    2 solutions added to tempConversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp-32) * 5/9
+
+  if(convertedTemp % 1 === 0){
+    return convertedTemp
+  }
+  else{
+    return +convertedTemp.toFixed(1)
+  }
+  
+  //or faster 
+  //convertedTemp = Math.round(convertedTemp * 10) / 10 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let convertedTemp = temp * 9/5 +32
+
+  if(convertedTemp % 1 === 0){
+    return convertedTemp
+  }
+  else{
+    return +convertedTemp.toFixed(1)
+  }
 };
 
 // Do not edit below this line
