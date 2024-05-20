8902445f1588136aeb344fa7c5bdfc0a7fa7af24
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-20 21:28:31 +0100

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word = "A car, a man, a maraca.") {
+    palindrome = ""
+    word = word.replace(/[^\w\s\']|_/g, "")
+        .replace(/\s+/g, "")
+        .toLowerCase()
+        .replace(" ");
+    for (i = word.length;i>0;i--){
+        palindrome = palindrome.concat(word[i-1])
+    }
+    console.log(word, " + ", palindrome)
+    return (word === palindrome);
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
