4a289ecbb35ef8b718a6825af532c7bbbdbd5294
austernotus <76696499+austernotus@users.noreply.github.com>
2024-05-20 21:59:15 +0100

    finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    listOfBooks = []
 
+    for (book in obj){
+        listOfBooks.push(obj[book].title)
+    }
+    return listOfBooks
 };
 
 // Do not edit below this line
